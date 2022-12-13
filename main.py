b08fad72161512fcb09d15afe225bc3d167d501c
José Eduardo <jose.edu.vs516@gmail.com>
2022-12-13 15:03:19 -0300

    gui start v

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tribute_class import tribute
 import funcs
+import tkinter as gui
 
+app = gui.Tk()
+app.title('Hanger Games GUI')
+app.geometry('800x600')
 
 world_map = funcs.world_map()
+
+app.mainloop()
