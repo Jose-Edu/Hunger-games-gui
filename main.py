e25a5c5ad40e69997a85bc510e7142e3a8ab3418
José Eduardo <jose.edu.vs516@gmail.com>
2024-03-31 17:34:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import tkinter as gui
-from windows import *
-import os
-import funcs
+#Importação das bibliotecas usadas
+import tkinter as gui #Interface gráfica
+from windows import * #Importa as classes referentes às diferentes possíveis telas do programa
+from os.path import dirname #Importa a função usada para a leitura do diretório atual
 
 
+#Cria a classe base do projeto
 class window_base:
 
+    #Configurações básicas da janela: Tamanho, título, ícone e etc.
     app = gui.Tk()
     app.title('Hunger Games GUI')
     app.iconbitmap('images//common//ico.ico')
@@ -14,16 +16,13 @@ class window_base:
     bg_cl = '#8B0000'
     frame = gui.Frame(app, background=bg_cl)
     frame.pack(fill='both', expand='yes')
-    tributes = []
     menu = gui.Menu(app)
-    day = 1
-    time = 'Dia'
-    game_mode = ''
-    round_deaths = 0
-    world_map = funcs.world_map()
 
 
+    #Configurações de inicialização init
     def __init__(self):
+
+        #Importação das diferentes telas
         self.mm = main_menu(self)
         self.st = show_tributes(self)
         self.game = game(self)
@@ -31,6 +30,7 @@ class window_base:
         self.ws = win_screen(self)
         self.ts = transition_screen(self)
 
+        #Declaração das variáveis de controle
         self.tributes = []
         self.menu = gui.Menu(self.app)
         self.day = 1
@@ -38,6 +38,7 @@ class window_base:
         self.game_mode = ''
         self.round_deaths = 0
 
+        #Criação do menu de visão aprimorada
         self.menu = gui.Menu(self.app)
         menu_actions = gui.Menu(self.menu, tearoff=0)
         menu_actions.add_command(label='Map', command=self.open_map)
@@ -45,17 +46,25 @@ class window_base:
         self.menu.add_cascade(label='Actions', menu=menu_actions)
 
 
+    #Função que limpa a tela do programa, destruindo o Frame atual e criando um novo
     def reset_window(self):
         self.frame.destroy()
         self.frame = gui.Frame(self.app, background=self.bg_cl)
         self.frame.pack(fill='both', expand='yes')
 
 
+    #Função que abre a tela secundária do mapa
     def open_map(self):
-        path = os.path.dirname(__file__)
+        path = dirname(__file__)
         exec(open(path+'\\map.py').read(), {'bg_cl': self.bg_cl, 'path': path, 'tributes': self.tributes})
 
+    #Função que abre a tela secundária das informações dos tributos
+    def open_tributes_info(self):
+        path = dirname(__file__)
+        exec(open(path+'\\tributes_info.py').read(), {'bg_cl': self.bg_cl, 'path': path, 'tributes': self.tributes}) 
 
+
+    #Função que retorna o número atual de tributos restantes no jogo
     def tributes_count(self):
         t_count = 24
 
@@ -66,6 +75,7 @@ class window_base:
         return t_count
 
 
+    #Função que atualiza o horário e, consequentemente, o contador de dias. Redefine as configurações para o próximo dia.
     def day_update(self):
         if self.time == 'Dia':
             self.time = 'Noite'
@@ -77,11 +87,6 @@ class window_base:
             t.update(self.tributes)
 
 
-    def open_tributes_info(self):
-        path = os.path.dirname(__file__)
-        exec(open(path+'\\tributes_info.py').read(), {'bg_cl': self.bg_cl, 'path': path, 'tributes': self.tributes}) 
-
-
+#Atribuição da classe e estrutura básica de Apps Tkinter
 _global_ = window_base()
-
 _global_.app.mainloop()
