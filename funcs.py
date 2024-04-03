81f51ce0dbb074a3556c96d971f8ce66ba52cdea
José Eduardo <jose.edu.vs516@gmail.com>
2024-04-03 16:45:02 -0300

    0.4/indev
    funcs.py update:
    *docs update
    *code rework

--- funcs.py
+++ funcs.py
@@ -60,12 +60,15 @@ def world_map():
 def images_set():
 
     """
-    
+    Função que define as imagens dos tributos,\n 
+    adicionando as enviadas pelo usuário e colocando genéricas quando nescessário.\n
+    Retorna um dicionário com todas as extenções das imagens, para facilitar consultas.
     """
 
     #Armazena o caminho do arquivo na variável path
     path = os.path.dirname(__file__)
 
+    #Cria o dicionário que guardará as extenções dos arquivos
     formats = {'25': [], '100': [], '200': []}
 
     #Caso ainda não exista, cria as pastas de imagens temporárias
@@ -75,78 +78,120 @@ def images_set():
         os.mkdir(path+'\\images\\temp\\100px')
         os.mkdir(path+'\\images\\temp\\200px')
 
+    #Laço principal que acha os arquivo os move às pastas certas, transformando suas resoluções
+    for c in range(1, 25): # c = Tributo atual do laço
 
-    for c in range(1, 25):
+        #Define o suposto caminho da imagem, sem sua extensão
         origin = path+f'\\images\\tributes\\{c}'
+
+        #Procura a extensão da imagem entre as opções aceitas e, ao achar, salva esse dado no formats.
         for t in ('.png', '.jpg', '.jpeg', '.gif'):
             if os.path.exists(origin+t):
                 for size in ('25', '100', '200'):
-                    origin += t
-                    destiny = path+f'\\images\\temp\\{size}px\\{c}{t}'
                     formats[size].append(t)
+                
+                #Aplica a extenção do arquivo nas vars que armazenam os paths
+                origin += t
+                destiny = path+f'\\images\\temp\\sizepx\\{c}{t}'
                 break
 
-        try:
+        try: #Copia os arquivos para as pastas certas e redefine suas resoluções
+            
             for size in ('25', '100', '200'):
-                copy(origin, destiny)
-                img = Image.open(destiny)
+                des = destiny.replace('size', size)
+                copy(origin, des)
+                img = Image.open(des)
                 _img = img.resize((int(size), int(size)))
-                _img.save(destiny)
-        except:
+                _img.save(des)
+
+        except: #Se o usuário não definir uma imagem, resultará em erro. Nesse caso, o except usará uma imagem genérica
             for size in ('25', '100', '200'):
                 origin = path+f'\\images\\common\\t{size}px.gif'
                 destiny = path+f'\\images\\temp\\{size}px\\{c}.gif'
                 formats[size].append('.gif')
                 copy(origin, destiny)
-                img = Image.open(destiny)
-                _img = img.resize((int(size), int(size)))
-                _img.save(destiny)
 
+    #Se a imagem não salva em '.png' ou '.gif', trasforma as suas copias em '.png'
     for size in ('25', '100', '200'):
         for c in range(1, 25):
+
             if formats[size][c-1] not in ('.png', '.gif'):
+
                 p = path+f'\\images\\temp\\{size}px\\{c}{formats[size][c-1]}'
                 i = Image.open(p)
                 i.save(path+f'\\images\\temp\\{size}px\\{c}.png')
                 os.remove(p)
                 formats[size][c-1] = '.png'
+
+    #Retorna a lista de extenções
     return formats
 
 
 def tributes_create(mode, tributes):
+
+    """
+    Função que cria cada um dos 24 tributos e armazena-os no argumento tributes.
+    """
+
+    #Armazena o caminho das imagens
     path = os.path.dirname(__file__)+'\\images'
 
+    # region nomes
+
+    #Cria a lista de nomes
     names = []
     n_file = open(path+'\\tributes\\tributes.txt', 'rt')
+
+    #Lê os nomes e os armazena na lista
     for c in range(0, 24):
         names.append(n_file.readline())
+
     n_file.close()
+
+    #Executa uma limpa no texto do nome, tirando espaços incorretos, quebra de linhas e adiciona nome genérico quando nescessário
     for c in range(0, 24):
+
         names[c] = names[c].strip()
+
         if names[c] == '' or names[c] == '\n':
             names[c] = 'T'+str(c+1)
+
         if names[c][(len(names[c])-3):] == '\n':
             names[c] = names[c][:(len(names[c])-3)]
+    # endregion
 
+    # region imagens
+
+    #Define os arquivos das imagens e define variáveis de controle
     formats = images_set()
     imgs = {'25': [], '100': [], '200': []}
 
+    #Armazena os paths de cada uma das imagens no dict imgs
     for c in ('25', '100', '200'):
         path_imgs = path+f'\\temp\\{c}px\\'
         for i in range(1, 25):
             imgs[c].append(path_imgs+str(i)+formats[c][i-1])
-    for c in range(0, 24):
+    # endregion
 
+    #Cria cada um dos tributos
+    for c in range(0, 24):
         tributes.append(tribute(names[c], c, imgs['25'][c], imgs['100'][c], imgs['200'][c], mode))
 
 
-def get_biome_locate(biome):
+def get_biome_locate(biomes):
+
+    """Função que retorna uma lista com todos pontos do mapa que sejam de um dos biomas passados.\n
+        biomes = lista de biomas com nomes conforme definido em world_map()
+    """
+
+    #Definições
     wm = world_map()
     locations = []
 
-    for l in range(0, 7):
-        for c in range(0, 7):
-            if wm[l][c] == biome:
+    #Checa célula por célula da matriz e, quando achar biomas corretos, adiciona a localização à lista locations
+    for l in range(0, 7): #l = Linha atual do laço
+        for c in range(0, 7): #c = Coluna atual do laço
+            if str(wm[l][c]) in biomes:
                 locations.append([l, c])
 
     return locations
@@ -188,8 +233,8 @@ def get_distance(dis1, dis2):
 
 
 def closest_biome(biome, locate):
-    ctrl = get_biome_locate(biome)[0]
-    for b in get_biome_locate(biome):
+    ctrl = get_biome_locate((biome))[0]
+    for b in get_biome_locate((biome)):
         if get_distance(locate, b) < get_distance(locate, ctrl):
             ctrl = b
         elif get_distance(locate, b) == get_distance(locate, ctrl):
@@ -275,3 +320,4 @@ def find_tribute(_tributes, _self, mode='random', distance=6, exceptions=[]):
                 elif _tributes[_self].relation[c] == _tributes[_self].relation[t]:
                     t = choice((t, c))
         return t
+    
